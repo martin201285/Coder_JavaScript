4a235bfd3a5eafcea31954d90cf4098d78337b37
martin201285 <martin.marron@outlook.com>
2022-11-26 10:26:47 -0300

    34110 - FIX - Se modifica el código js para adaptarlo a una función

--- js/index.js
+++ js/index.js
@@ -1,14 +1,32 @@
-let valor = parseInt(prompt("1) Ingrese un valor en pesos (ARS)."));
-let moneda = prompt("2) Ingrese USD para convertir a dólar o EUR para convertir a euro.").toLowerCase();
-let cotizacionUsd = 171.75;
-let cotizacionEur = 176.50;
+function conversor() {
 
-if ((valor > 0) && (moneda === "usd")) {
-    valor = valor * cotizacionUsd
-    alert("El valor es ARS " + valor + " a cotización oficial de " + cotizacionUsd);
-} else if ((valor > 0) && (moneda === "eur")) {
-    valor = valor * cotizacionEur
-    alert("El valor es ARS " + valor + " a cotización oficial de " + cotizacionEur);
-} else {
-    alert("Por favor, ingrese un valor válido.")
+    let valor = parseInt(prompt("1) Ingrese un valor en pesos (ARS)."));
+    let moneda = prompt("2) Seleccione: USD para dólar, EUR para euro, GBP para libra, BRL para real.").toLowerCase();
+
+    // Cotizaciones
+    let cotizacionUsd = 165.31; // Dólar
+    let cotizacionEur = 176.50; // Euro
+    let cotizacionGbp = 199.87; // Libra esterlina
+    let cotizacionBrl = 6.54; // Real
+
+    let resultado = 0;
+
+    if ((valor > 0) && (moneda === "usd")) {
+        resultado = valor * cotizacionUsd;
+        alert("El valor es ARS " + resultado + " a cotización oficial de " + cotizacionUsd);
+        } else if ((valor > 0) && (moneda === "eur")) {
+        resultado = valor * cotizacionEur;
+        alert("El valor es ARS " + resultado + " a cotización oficial de " + cotizacionEur);
+        } else if ((valor > 0) && (moneda === "gbp")) {
+        resultado = valor * cotizacionGbp;
+        alert("El valor es ARS " + resultado + " a cotización oficial de " + cotizacionGbp);
+        } else if ((valor > 0) && (moneda === "brl")) {
+        resultado = valor * cotizacionBrl;
+        alert("El valor es ARS " + resultado + " a cotización oficial de " + cotizacionBrl);
+        } else {
+        alert("Por favor, ingrese un valor válido.");
+        
+    }
 }
+
+conversor();
