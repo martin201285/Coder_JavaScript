629901fb8274262f44f63ecd381722a7c92103b4
martin201285 <martin.marron@outlook.com>
2023-01-16 18:00:19 -0300

    34110 - FEAT - Se agrega boton con libreria SweetAlert para el proyecto

--- js/index.js
+++ js/index.js
@@ -243,6 +243,27 @@ function scrollUp(pxScreen){
 
 scrollUp(100);
 
+// SWEETALERT ON OLD CONVERSOR BUTTON
+
+oldConversor.addEventListener('click', () => {
+    const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.addEventListener('mouseenter', Swal.stopTimer)
+          toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+      })
+      
+      Toast.fire({
+        icon: 'error',
+        title: 'Old Conversor is no longer available.'
+      })
+})
+
 
 // // CONSTRUCTOR: CURRENCY
 
